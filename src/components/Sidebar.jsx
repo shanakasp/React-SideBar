129131a97481610125a787797138b886c357cc2c
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-23 11:57:50 +0530

    changed 3 files

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Icon from "../Images/Icon.svg"
+import Profile from "../Images/profile.png"
+import Dashboard from "../Images/dashboard.svg"
+import Transactions from "../Images/transactions.svg"
+import Performance from "../Images/performance.svg"
+import News from "../Images/news.svg"
+import Settings from "../Images/settings.svg"
+import Support from '../pages/Support'
 
-function Sidebar() {
+
+const Sidebar=() => {
   return (
-    <div></div>
+    <div className='sidebar'>
+    <div className='logoContainer'>
+     <img src={Icon} alt='Icon' className='logo' /></div>
+     <h2 className='title'>Prince SP</h2>
+     <div className='burgerContainer'>
+     <div className='burgerTrigger'></div>
+     <div className='burgerMenu'></div> 
+     </div>
+    <div className='profileContainer'>
+      <img src={Profile} alt='profile' className='profile'/>
+      <p className='name'>Hello Shanka</p>
+      <p>shanakaprince@gmail</p>
+    </div>
+
+
+     </div>
+
+   
   )
 }
 
